aca51343b79e97ef435d888b22c12b1135827856
Raja Shravan <rajashravan@hotmail.com>
2020-04-19 17:21:07 -0400

    adds contact edit ability

--- crm/contacts/urls.py
+++ crm/contacts/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     # ex: /contacts/5/
     path('<int:contact_id>/', views.detail, name='detail'),
+    path('<int:contact_id>/edit', views.contact_edit_view, name='edit'),
     path('create', views.contact_create_view, name='create')
 ]
--- crm/contacts/views.py
+++ crm/contacts/views.py
@@ -9,7 +9,7 @@ from .forms import ContactForm
 
 @login_required(login_url="/login/")
 def index(request):
-    contact_list = Contact.objects.order_by('-first_name')[:5]
+    contact_list = Contact.objects.order_by('first_name')
     context = {'contact_list': contact_list}
     return render(request, 'index.html', context)
 
@@ -27,8 +27,17 @@ def contact_create_view(request):
         contact.save()
         return HttpResponseRedirect(reverse('contacts:detail', args=(contact.id,)))
 
-    context = {
-        'form': form
-    }
+    return render(request, 'pages/contact_create.html', {'form': form})
 
-    return render(request, 'pages/contact_create.html', context)
+def contact_edit_view(request, contact_id):
+    contact = get_object_or_404(Contact, id=contact_id)
+    if request.method == "POST":
+        form = ContactForm(request.POST, instance=contact)
+        if form.is_valid():
+            contact = form.save(commit=False)
+            contact.user = request.user
+            contact.save()
+            return HttpResponseRedirect(reverse('contacts:detail', args=(contact.id,)))
+    else:
+        form = ContactForm(instance=contact)
+    return render(request, 'pages/contact_create.html', {'form': form})
